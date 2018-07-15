9976abc97eabed45888b85c561bc5c7f940a9847
Matt <matthewgstillman@gmail.com>
2018-07-15 15:30:38 -0700

    Latest Commit

--- apps/google_maps_api/models.py
+++ apps/google_maps_api/models.py
@@ -2,4 +2,18 @@ from __future__ import unicode_literals
 
 from django.db import models
 
+class UserManager(models.Manager):
+    def login(self, postData):
+        return self
+
+    def create_trip(self, postData):
+        starting_address = postData['starting_address']
+        ending_address = postData['ending_address']
+        return self
+
+class Trip(models.Model):
+    starting_address = models.CharField(max_length=38)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    objects = UserManager()
 # Create your models here.
--- apps/google_maps_api/views.py
+++ apps/google_maps_api/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 import googlemaps
+import requests
 from datetime import datetime
 
 # Create your views here.
@@ -12,8 +13,8 @@ def index(request):
     reverse_geocode_result = gmaps.reverse_geocode((40.714224, -73.961452))
     # Request directions via public transit
     now = datetime.now()
-    directions_result = gmaps.directions("Sydney Town Hall",
-                                     "Parramatta, NSW",
+    directions_result = gmaps.directions("245 N 11th St, San Jose, CA 95112",
+                                     "711 2nd Ave, Salt Lake City, UT, 84103",
                                      mode="driving",
                                      departure_time=now)
     print(now)
@@ -29,3 +30,41 @@ def index(request):
         'reverse_geocode_result': reverse_geocode_result,
     }
     return render(request, 'google_maps_api/index.html', context)
+
+def enter_trip(request):
+    if request.method == 'GET':
+        gmaps = googlemaps.Client(key='AIzaSyBCpcT3_3H3Qd7dZ05uK_HqoQ2id7fz8M8')
+        # Geocoding an address
+        geocode_result = gmaps.geocode('1600 Amphitheatre Parkway, Mountain View, CA')
+        # Look up an address with reverse geocoding
+        reverse_geocode_result = gmaps.reverse_geocode((40.714224, -73.961452))
+        # Request directions via public transit
+        now = datetime.now()
+        directions_result = gmaps.directions("Sydney Town Hall",
+                                        "Parramatta, NSW",
+                                        mode="driving",
+                                        departure_time=now)
+        # print(now)
+        # print(directions_result)
+        # print(geocode_result)
+        # print(gmaps)
+        # print(reverse_geocode_result)
+        context = {
+            'directions_result': directions_result,
+            'geocode_result': geocode_result,
+            'gmaps': gmaps,
+            'now': now,
+            'reverse_geocode_result': reverse_geocode_result,
+        }
+        return render(request, 'google_maps_api/enter_trip.html', context)
+    if request.method == 'POST':
+        print request.POST
+        User.objects.create_trip(request.POST)
+        # starting_address = request.POST['starting_address']
+        # print starting_address
+        # ending_address = request.POST['ending_address']
+        # print ending_address
+        context = {
+            'trip': trip,
+        }
+    return render(request, 'google_maps_api/enter_trip.html', context)
