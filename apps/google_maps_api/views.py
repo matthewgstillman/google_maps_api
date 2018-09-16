e3165fcbf0592420b00d08a9aff63c0f59c15f1f
Matt <matthewgstillman@gmail.com>
2018-09-15 17:50:03 -0700

    Added Street View

--- apps/google_maps_api/views.py
+++ apps/google_maps_api/views.py
@@ -11,25 +11,15 @@ from api_key import api_key
 def index(request):
     session_formatted_starting_address = request.session['formatted_starting_address']
     session_formatted_ending_address = request.session['formatted_ending_address']
-    print("Formatted Ending Address: " + str(session_formatted_ending_address))
     session_geocode_ending_address = request.session['geocode_ending_address']
-    print("Session Geocode Ending Address: " + str(session_geocode_ending_address))
     latitude = session_geocode_ending_address[0]['geometry']['location']['lat']
     longitude = session_geocode_ending_address[0]['geometry']['location']['lng']
     lat_lon = str(latitude) + "," + str(longitude)
     key2 = api_key['api_key2']
-    print("Key 2: " + str(key2))
     starting_address = request.session['starting_address']
-    print("Starting Address: " + str(starting_address))
     ending_address = request.session['ending_address']
-    print("Ending Address: " + str(ending_address))
     key = api_key['api_key']
     gmaps = googlemaps.Client(key=key)
-    # Geocoding an address
-    geocode_result = gmaps.geocode('1600 Amphitheatre Parkway, Mountain View, CA')
-    # Look up an address with reverse geocoding
-    reverse_geocode_result = gmaps.reverse_geocode((40.714224, -73.961452))
-    # Request directions via public transit
     now = datetime.now()
     directions_result = gmaps.directions(starting_address,
                                      ending_address,
@@ -39,12 +29,10 @@ def index(request):
         'starting_address': starting_address,
         'ending_address': ending_address,
         'directions_result': directions_result,
-        'geocode_result': geocode_result,
         'key2': key2,
         'lat_lon': lat_lon,
         'gmaps': gmaps,
         'now': now,
-        'reverse_geocode_result': reverse_geocode_result,
         'session_formatted_starting_address': session_formatted_starting_address,
         'session_formatted_ending_address': session_formatted_ending_address,
         'session_geocode_ending_address': session_geocode_ending_address,
@@ -55,11 +43,6 @@ def enter_trip(request):
     if request.method == 'GET':
         key = api_key['api_key']
         gmaps = googlemaps.Client(key=key)
-        # Geocoding an address
-        geocode_result = gmaps.geocode('1600 Amphitheatre Parkway, Mountain View, CA')
-        # Look up an address with reverse geocoding
-        reverse_geocode_result = gmaps.reverse_geocode((40.714224, -73.961452))
-        # Request directions via public transit
         now = datetime.now()
         directions_result = gmaps.directions("245 N 11th St, San Jose, CA 95112",
         "711 2nd Ave, Salt Lake City, UT, 84103",
@@ -67,11 +50,9 @@ def enter_trip(request):
         departure_time=now)
         context = {
             'directions_result': directions_result,
-            'geocode_result': geocode_result,
             'gmaps': gmaps,
             'key': key,
             'now': now,
-            'reverse_geocode_result': reverse_geocode_result,
         }
         return render(request, 'google_maps_api/enter_trip.html', context)
     if request.method == 'POST':
